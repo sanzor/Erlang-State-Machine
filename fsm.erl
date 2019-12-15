1dc04e249d5b9be56d6db662a97e47e592da11ea
sanzor <adriansan_93@yahoo.com>
2019-12-15 23:26:49 +0200

    solve handle event functions

--- fsm.erl
+++ fsm.erl
@@ -1,5 +1,5 @@
 -module(fsm).
--record(state,{
+-record(data,{
     current="None",
     intvCount=0,
     jobCount=0
@@ -9,15 +9,18 @@
 -export([state/1,start/0,hire/2,fire/2,interview/2]).
 
 -export([sitting_home/3,interviewing/3,working/3]).
--export([handle_event/4]).
-
+-export([handle_event/3]).
 -behaviour(gen_statem).
 
+handle_event({call,From},get_state,Data)->
+    {keep_state,Data,[{reply,Data}]};
+handle_event({call,From},Event,Data)->
+    {keep,state,Data}.
 %API
 start()->
     gen_statem:start_link(?MODULE,[],[]).
 state(PID)->
-    gen_statem:call(PID,state).
+    gen_statem:call(PID,get_state).
 hire(PID,Company)->
     gen_statem:call(PID,{hire,Company}).
 fire(PID,Company)->
@@ -28,39 +31,30 @@ interview(PID,Company)->
 %mandatory
 code_change(V,State,Data,Extra)->{ok,State,Data}.
 callback_mode() ->
-    [state_functions,handle_event_function].
+    state_functions,.
 init([])->
-    {ok,sitting_home,#state{current="None",jobCount=0,intvCount=0}}.
+    {ok,sitting_home,#data{current="None",jobCount=0,intvCount=0}}.
 terminate(Reasom,State,Data)->
     void.
 
-% Generic handlers
-handle_event({call,From},state,State,Data)->
-    {keep_state,State,[{reply,From,Data}]};
-handle_event(_,_,State,Data)->
-    {keep_state,State}.
-
 % State implementations
-sitting_home({call,From},{intv,Company},State=#state{intvCount=C})->
-    {next_state,interviewing,State#state{intvCount=C+1},[{reply,From,"Interviewing..."}]};
-sitting_home(EventType,Event,State)->
-    handle_event(EventType,Event,State,[]).
-interviewing({call,From},{rejected,Company},State)->
-    {next_state,sitting_home,State,[{reply,From,"rejected by:"++Company}]};
-interviewing({call,From},{accepted,Company},State=#state{jobCount=J})->
-    {next_state,
-    working,
-    State#state{jobCount=J+1,current=Company},
-    [{reply,From,"accepted offer from:"++Company}]
-};
-interviewing(EventType,Event,State)->
-    handle_event(EventType,Event,State,[]).
+sitting_home({call,From},{intv,Company},Data=#data{intvCount=C})->
+    io:format("sugi pl"),
+    {next_state,interviewing,Data#data{intvCount=C+1}};
+sitting_home({call,From},Event,Data)->
+    {keep_state,Data}.
+interviewing({call,From},{rejected,Company},Data)->
+    {next_state,sitting_home,Data};
+interviewing({call,From},{accepted,Company},State=#data{jobCount=J})->
+    {next_state,working,State#data{jobCount=J+1,current=Company}};
+interviewing(EventType,Event,Data)->
+    {keep_state,Data}.
 
 
-working({call,From},{fire,Company},State=#state{current=C})->
-    {next_state,working,State#state{current="None"},[{reply,From,"Got fired from"++Company}]};
-working(EventType,Event,State)->
-      handle_event(EventType,Event,State,[]).
+working({call,From},{fire,Company},Data=#data{current=C})->
+    {next_state,working,Data#data{current="None"},[{reply,From,"Got fired from"++Company}]};
+working(EventType,Event,Data)->
+    {keep_state,Data}.
 
 
 
