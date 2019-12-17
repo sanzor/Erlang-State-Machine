35e6427b53589b761e02c275fa48fc50359046b1
sanzor <adriansan_93@yahoo.com>
2019-12-17 18:36:28 +0200

    Wooorks 1

--- fsm.erl
+++ fsm.erl
@@ -2,21 +2,22 @@
 -record(data,{
     current="None",
     intvCount=0,
-    jobCount=0
+    jobCount=0,
+    state="None"
 }).
 -export([init/1,terminate/3,callback_mode/0,code_change/4]).
 
 -export([state/1,start/0,interview/2,reject/2,wait/1]).
 
 -export([sitting_home/3,interviewing/3]).
--export([handle_event/3]).
+-export([handle/3]).
 -behaviour(gen_statem).
 
 %API
 start()->
     gen_statem:start_link(?MODULE,[],[]).
 state(PID)->
-    gen_statem:call(PID,get_state).
+    gen_statem:call(PID,state).
 
 interview(PID,Company)->
     gen_statem:call(PID,{intv,Company}).
@@ -37,27 +38,24 @@ terminate(Reasom,State,Data)->
 % State implementations
 
 sitting_home({call,From},{intv,Company},Data=#data{intvCount=C})->
-    gen_statem:reply(From,{got_interview,Company}),
-    {next_state,interviewing,Data#data{intvCount=C+1}};
+    {next_state,interviewing,Data#data{intvCount=C+1},[{reply,From,{got_interview,Company}}]};
 sitting_home(EventType,Event,Data)->
-    handle_event(EventType,Event,Data).
+    handle(EventType,Event,Data).
 
 interviewing({call,From},{rejected,Company},Data)->
-    gen_statem:reply(From,{got_rejected,Company}),
-    {next_state,sitting_home,Data}};
+    {next_state,interviewing,Data,[{reply,From,{rejected,Company}}]};
 interviewing(EventType,Event,Data)->
-    handle(EventType,nada,Data);
-interviewing({call,From},state,Data)->
-    handle({call,From},)
+    handle(EventType,Event,Data).
+
 
 handle({call,From},state,Data)->
-    gen_statem:reply(From,Data),
-    {keep_state,Data};
-handle(EventType,EventContent,Data)->
+    {keep_state,Data,[{reply,From,Data}]};
+handle({call,From},State,Data)->
+    {keep_state,Data,[{reply,From,got_nothing_for_you}]};
+handle(_,State,Data)->
     {keep_state,Data}.
-getState(From,State,Data)->
-    gen_statem(From,{State,Data}),
-    {keep_state,Data}
+
+
 
 
 
